64895a2f824e008d2e6723773a9500df989203fd
Xiaofeng-997 <152526314+Xiaofeng-997@users.noreply.github.com>
2023-11-30 18:39:55 +0100

    Uppgifter  för veckan 1-2

---
+++ Homework Week 1-2.py
@@ -0,0 +1,26 @@
+#Tilldelade en sträng i name med '' tecken
+name = 'xiaofeng'
+
+#Tilldelade en heltal i age
+age = 26
+
+#Tilldela en flyttal i height
+height = 170.01
+
+#Genom print(f'') kan jag skriva ut en mening med tidigare skapade variablerna
+print(f'Mitt namn är {name.title()}. Jag är {age} år gammal och jag är {height} cm lång')
+
+#Här skapade jag ett list som innehåller sträng, interger och float
+mix_list=['name',5,5.55,]
+print(mix_list)
+
+number_string = '5'#Här tilldela jag en sträng
+number = int(number_string)#Int()för omvandla en sträng till integer
+number2 = '5.59999'
+number3 = float(number2)#float används när det har decimal
+print(number,number3)
+
+I_M_A_Beginner = True
+print(type(I_M_A_Beginner))#Här skriver ut jag en bool datatypen
+print(type(age))#Här skriver ut jag en integer datatypen
+print(type(height))#Här skriver ut jag en float datatypen
